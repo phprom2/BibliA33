55f16b6248b2b9ffad84006fb72502f556de1ed3
phprom2 <100920245+phprom2@users.noreply.github.com>
2023-01-25 16:39:12 +0100

    Ajout requête préparé
    Page LoginClient

--- newBackOffice/loginClient.php
+++ newBackOffice/loginClient.php
@@ -1,28 +1,29 @@
 <?php
-
-$login=($_POST["login"]);
-$pwd= md5(($_POST["pwd"]));
+$login = ($_POST["login"]);
+$pwd = md5(($_POST["pwd"]));
 
 require '../sqlconnect.php';
-$sql = "SELECT *  FROM client WHERE CLIENT_MAIL =  '".$login."' AND CLIENT_MDP ='".$pwd."'";
-ECHO $sql;
-$table = $connection->query($sql);
-$count=$table->rowCount();
-ECHO $count;
+$sql = "SELECT *  FROM client WHERE CLIENT_MAIL = ? AND CLIENT_MDP = ?";
+
+$stmt = $connection->prepare($sql);
+$stmt->bindValue(1, $login);
+$stmt->bindValue(2, $pwd);
+$stmt->execute();
+$result = $stmt->fetch();
+$count = $stmt->rowCount();
+
 if ($count > 0) {
-	$ligne = $table->fetch();
-	session_start();
-	$_SESSION['CLIENT_MAIL']=$login;
-    $_SESSION['CLIENT_ID']=$ligne['CLIENT_ID'];
+    session_start();
+    $_SESSION['CLIENT_MAIL']=$login;
+    $_SESSION['CLIENT_ID']=$result['CLIENT_ID'];
     header('Location: accueilClient.php');
-}
-else{
-	?>
-	<script>alert("Erreur: Nom d'utilisateur ou mot de passe incorrect.");
+} else {
+    ?>
+    <script>alert("Erreur: Nom d'utilisateur ou mot de passe incorrect.");
             window.location.href = "../connexionClient.php";
-	</script>
-	<?php
-        
+    </script>
+    <?php
 }
+
 ?>
 
